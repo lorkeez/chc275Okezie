d3cb8572daac27661ae444425d2b9a0ba90ad8d6
lorkeez <OkezieC27@chcstudent.com>
2025-09-14 19:24:18 -0400

    commit

--- calc.py
+++ calc.py
@@ -4,8 +4,21 @@ num2 = input("Enter number 2: ")
 num2 = int(num2)
 sum = num1 + num2
 print(f"The sum of {num1} and {num2} is {sum}")
-num3 = input("Enter your number 3: ")
-num4 = input("Enter your number 4: ")
+num3 = input("Enter number 3: ")
+num4 = input("Enter number 4: ")
 num3 = int(num3)
 num4 = int(num4)
 sum = num3 - num4
+print(f"The sum of {num3} and {num4} is {sum}")
+num5 = input("Enter number 5: ")
+num5 = int(num5)
+num6 = input("Emter mumber 6: ")
+num6 = int(num6)
+sum = num5 * num6
+print(f"The sum of {num5} amd {num6} is {sum}")
+num7 = input("Enter number 7: ")
+num7 = int(num7)
+num8 = input("Enter number 8: ")
+num8 = int(num8)
+sum = num7 / num8
+print(f"The sum of {num7} and {num8} is {sum}")
