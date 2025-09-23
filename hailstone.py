098a9118fba4c5e2821a5749945ce790e0e1fcc8
coveys880 <coveys27@chcstudent.com>
2025-09-23 11:34:32 -0400

    commit

---
+++ hailstone.py
@@ -0,0 +1,11 @@
+x = input("What is the number?")
+x = int(x)
+check = False
+while check == False:
+    print(x)
+    if x == 1:
+        check = True
+    if x % 2 == 1:
+        x = 3*x + 1
+    if x % 2 == 0:
+        x = x/2
