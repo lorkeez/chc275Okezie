f0110fa87dff58766f483c5e3bef247e24e0caf0
coveys880 <coveys27@chcstudent.com>
2025-10-23 14:42:21 -0400

    c

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+reverse = ""
+pal = input("What is your word").strip().lower()
+for char in pal:
+    reverse = char + reverse
+if reverse == pal:
+    print("it is a palindrome")
---
+++ trial.py
@@ -0,0 +1,12 @@
+word = "school"
+temp = ""
+for char in word:
+    temp = temp + char
+    print(temp)
+    
+    
+str1 = "heloo"
+str2 = "world"
+
+str2 + str1 
+str1 + str2 
